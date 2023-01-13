a6b70a0f419b8349082e5b86dd32bda72e403b8a
Corbo <ccorbo@connexcu.org>
2023-01-13 16:21:58 -0500

    Added get images function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Meme from "./components/Meme"
 
 export default function App(){
+
   return(
     <div>
       <Header/>
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,19 +3,24 @@ import memesData from "../memesData"
 
 
 export default function Meme() {
-    function getMemeImage(){
+  
+    const [image, setImage] = React.useState("")
+    
+    function getImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         console.log(randomNumber)
         const url = memesArray[randomNumber].url
         console.log(url)
+        setImage(prevCount=>url)
     }
     return (
         <main>
             <form className="form">
                 <input type="text" placeholder="Top text" className="form--input"/>
                 <input type="text" placeholder="Bottom text" className="form--input"/>
-                <button className="form--button" onClick={getMemeImage} type="button"> Get a new meme image 🖼</button>
+                <button className="form--button" onClick={getImage}  type="button"> Get a new meme image 🖼</button>
+                <img src={image}></img>
             </form>
         </main>
     ) 
