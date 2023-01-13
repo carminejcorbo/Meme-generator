977ea8a6bbe8e894631f071466d564d451c14da1
Corbo <ccorbo@connexcu.org>
2023-01-13 11:56:22 -0500

    Added images , and some other updates.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Face from "../images/face.png"
 
 export default function Header(){
     return (
         <div className="header">
-        <img src="#"></img>
+        <img src={Face} className="header--image"></img>
         <h3 className="header--title">Meme Generator</h3>
         <h4 className="header--project">React Course - Project 3</h4>
         </div>
