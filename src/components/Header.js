67911a419c0f9fd3e10c8bb13ee985f7e80e89ce
Corbo <ccorbo@connexcu.org>
2023-01-13 09:53:19 -0500

    Started Project

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <div className="header">
+        <img src="#"></img>
+        <h3 className="header--title">Meme Generator</h3>
+        <h4 className="header--project">React Course - Project 3</h4>
+        </div>
+    )
+}
