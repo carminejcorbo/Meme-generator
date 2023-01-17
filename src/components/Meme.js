2b921b1829cc743e0c6eea5fa66482bdecfff14d
Corbo <ccorbo@connexcu.org>
2023-01-17 14:04:18 -0500

    Added some new css.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,24 +4,25 @@ import memesData from "../memesData"
 
 export default function Meme() {
   
-    const [image, setImage] = React.useState("")
+    const [image, setImage] = React.useState("https://i.imgflip.com/30b1gx.jpg")
     
     function getImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = memesData.data.memes // copy the array to memesArray from the js file.
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         console.log(randomNumber)
-        const url = memesArray[randomNumber].url
+        const url = memesArray[randomNumber].url // this is getting are url from the array of URLs
         console.log(url)
         setImage(prevCount=>url)
     }
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top text" className="form--input"/>
                 <input type="text" placeholder="Bottom text" className="form--input"/>
                 <button className="form--button" onClick={getImage}  type="button"> Get a new meme image 🖼</button>
-                <img src={image}></img>
-            </form>
+                
+            </div>
+            <img src={image} className="meme--image"></img>
         </main>
     ) 
 }
